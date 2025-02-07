e8d3b416b4de2283c4e837a84ed282fe2e1fe1e5
Stefan Keller <aegiskeller@gmail.com>
2025-02-07 01:42:50 +0000

    Update collect_radar.py

--- collect_radar.py
+++ collect_radar.py
@@ -1,5 +1,6 @@
 from coreWeather import download_radar, transform_radar, count_rain_pixels, store_rain_pixels
 from time import sleep
+import shutil
 
 site = 'IDR403' # Captain's Flat NSW
 #site = 'IDR163' # Pt Headland WA
@@ -13,4 +14,10 @@ while True:
     pixel_cnt = count_rain_pixels(256,256,100)
 
     store_rain_pixels(pixel_cnt, site)
+
+    source_folder = '/home/aegiskeller/cherax-radar/static'
+    destination_folder = '/var/www/hmtl'
+
+    # Copy the entire folder to the destination
+    shutil.copytree(source_folder, destination_folder)    
     sleep(300)
