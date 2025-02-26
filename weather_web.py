6f3de323c650a87f3b5d434cd4cf890dcf97700d
Stefan Keller <aegiskeller@gmail.com>
2025-02-26 04:01:49 +0000

    abs path

--- weather_web.py
+++ weather_web.py
@@ -2,11 +2,12 @@ from coreWeather import download_radar, transform_radar, count_rain_pixels, stor
 import os
 # the site is Captain's Flat NSW IDR403
 site = "IDR403"
+path = os.getcwd()
 # run the functions in order
-download_radar(site+".gif", "static/radar"+site+".gif")
-transform_radar("static/radar"+site+".gif")
+download_radar(site+".gif", path+"/static/radar"+site+".gif")
+transform_radar(path+"/static/radar"+site+".gif")
 count = count_rain_pixels(256,256,100)
 store_rain_pixels(count, site)
 plot_rain_pixels(site)
 rec_str = recommended_action(site)
-generate_web_page(rec_str, os.getcwd())
+generate_web_page(rec_str, path)
